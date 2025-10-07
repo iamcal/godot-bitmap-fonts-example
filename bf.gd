8372138dd214d1b2789063f3e1afe27e452821a1
Cal Henderson <cal@iamcal.com>
2025-10-07 09:55:26 -0700

    removed the original bitmapfont node and code

--- bf.gd
+++ bf.gd
@@ -23,16 +23,16 @@ func update_texture():
 	set_cache_descent(_cache_index, _size.x, 0)
 	#update_glyphs()
 
-func add_glyphs(characters: String, offset_x: int, offset_y: int, width: int, height: int):
-
-	for i in range (0, characters.length()):
-
-		var glyph = characters.to_ascii_buffer()[i]
-		var advance = width
-		var rect = Rect2(offset_x+(i*width), offset_y, width, height)
-		
-		font.set_glyph_uv_rect(_cache_index, _size, glyph, rect)
-		font.set_glyph_texture_idx(_cache_index, _size, glyph, _texture_index)
-		font.set_glyph_offset(_cache_index, _size, glyph, Vector2(0, -height))
-		font.set_glyph_size(_cache_index, _size, glyph, rect.size)
-		font.set_glyph_advance(_cache_index, _size.x, glyph, Vector2(advance, 0))
+#func add_glyphs(characters: String, offset_x: int, offset_y: int, width: int, height: int):
+#
+	#for i in range (0, characters.length()):
+#
+		#var glyph = characters.to_ascii_buffer()[i]
+		#var advance = width
+		#var rect = Rect2(offset_x+(i*width), offset_y, width, height)
+		#
+		#font.set_glyph_uv_rect(_cache_index, _size, glyph, rect)
+		#font.set_glyph_texture_idx(_cache_index, _size, glyph, _texture_index)
+		#font.set_glyph_offset(_cache_index, _size, glyph, Vector2(0, -height))
+		#font.set_glyph_size(_cache_index, _size, glyph, rect.size)
+		#font.set_glyph_advance(_cache_index, _size.x, glyph, Vector2(advance, 0))
--- root.gd
+++ root.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-const BITMAP_FONT = preload("res://BitmapFont.tscn")
+#const BITMAP_FONT = preload("res://BitmapFont.tscn")
 
 func _ready() -> void:
-	var font = BITMAP_FONT.instantiate()
-	font.set_texture(preload("res://assets/fonts.png"), 17)
-	font.add_glyphs("ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789&#<!>.", 3, 3, 17, 17)
-	font.add_glyphs("abcdefghijklmnopqrstuvwxyz", 3, 3, 17, 17)
-	
-	$Label.set("theme_override_fonts/font", font.font)
-	$Label.set("theme_override_font_sizes/font_size", font.size)
+	pass
+	#var font = BITMAP_FONT.instantiate()
+	#font.set_texture(preload("res://assets/fonts.png"), 17)
+	#font.add_glyphs("ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789&#<!>.", 3, 3, 17, 17)
+	#font.add_glyphs("abcdefghijklmnopqrstuvwxyz", 3, 3, 17, 17)
+	#
+	#$Label.set("theme_override_fonts/font", font.font)
+	#$Label.set("theme_override_font_sizes/font_size", font.size)
