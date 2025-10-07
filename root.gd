51c488f7026613f6f36c9103ec0da58d4791d355
Cal Henderson <cal@iamcal.com>
2025-10-07 09:38:48 -0700

    initial checkin - working demo with clunky UI

---
+++ root.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+const BITMAP_FONT = preload("res://BitmapFont.tscn")
+
+func _ready() -> void:
+	var font = BITMAP_FONT.instantiate()
+	font.set_texture(preload("res://assets/fonts.png"), 17)
+	font.add_glyphs("ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789&#<!>.", 3, 3, 17, 17)
+	font.add_glyphs("abcdefghijklmnopqrstuvwxyz", 3, 3, 17, 17)
+	
+	$Label.set("theme_override_fonts/font", font.font)
+	$Label.set("theme_override_font_sizes/font_size", font.size)
